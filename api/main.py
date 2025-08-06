65b331fd055177581974801968fd12622526542c
thomas-god <thomas.godlewski@icloud.com>
2025-08-06 11:29:41 +0200

    infra: scaffold api

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from api!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello world"}
