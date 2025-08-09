070844d09379867860cac5dc5f330f55a67753f0
thomas-god <thomas.godlewski@icloud.com>
2025-08-09 18:03:21 +0200

    feat(client): basic track upload page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { PUBLIC_API_URL } from '$env/static/public';
+
+  let files: FileList | undefined = $state();
+  let can_send = $derived.by(() => {
+    if (files) {
+      return files.length > 0;
+    }
+    return false;
+  });
+
+  const postTrack = async () => {
+    if (files === undefined) {
+      return;
+    }
+    if (files.length === 0) {
+      return;
+    }
+    const body = await files.item(0)!.bytes();
+
+    let res = await fetch(`${PUBLIC_API_URL}/track`, { body, method: 'POST' });
+    console.log(res);
+  };
+</script>
+
+<div class="mt-3 flex flex-col items-center gap-3">
+  <label for="track">Upload a .gpx file:</label>
+  <input accept=".gpx" bind:files id="track" name="track" type="file" class="file-input" />
+  <button class="btn btn-primary" disabled={!can_send} onclick={postTrack}>Send</button>
+</div>
