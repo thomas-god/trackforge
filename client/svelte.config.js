cc3f31e1f3a016235215aaba61f09f7870720f54
thomas-god <thomas.godlewski@icloud.com>
2025-08-11 16:51:56 +0200

    chore(client): fix formatting

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -3,16 +3,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
       precompress: false,
-      strict: true,
-    }) }
+      strict: true
+    })
+  }
 };
 
 export default config;
